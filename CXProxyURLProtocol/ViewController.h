403cba351f4312d8ed6f180e5e6eccffec966b72
轩哥 <misscxuan@163.com>
2018-07-20 10:45:04 +0800

    创建项目。

---
+++ CXProxyURLProtocol/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CXProxyURLProtocol
+//
+//  Created by Felix on 2018/7/20.
+//  Copyright © 2018年 CXTretar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CXProxyURLProtocolTests/CXProxyURLProtocolTests.m
@@ -0,0 +1,39 @@
+//
+//  CXProxyURLProtocolTests.m
+//  CXProxyURLProtocolTests
+//
+//  Created by Felix on 2018/7/20.
+//  Copyright © 2018年 CXTretar. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CXProxyURLProtocolTests : XCTestCase
+
+@end
+
+@implementation CXProxyURLProtocolTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
